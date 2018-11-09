b1f3913333d978fab89cfd063b69d4c25627443a
Kody Wilson <kodywilson@gmail.com>
2018-11-09 14:52:24 -0800

    Doing some of the lp3thw exercises for a python refresher.

---
+++ ex3_calc.py
@@ -0,0 +1,4 @@
+print("Let's calculate some stuff.")
+print("What % of games have the Seahawks won?", (4 / 8) * 100)
+print("modulus example, 100 % 16 = ", 100 % 16)
+print(100 % 16, "is left over.")
