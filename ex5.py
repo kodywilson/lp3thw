219b02db4904105c1d94a1195f5662d1342e5d70
Kody Wilson <kodywilson@gmail.com>
2018-11-15 12:43:36 -0800

    exercise 5

---
+++ ex5.py
@@ -0,0 +1,21 @@
+my_name = 'Kody Wilson'
+my_age = 41 # not a lie
+my_height = 70 # inches
+my_height_cm = my_height * 2.54
+my_weight = 185 # lbs
+my_weight_kg = my_weight * 0.45
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = "Silver"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("That's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_weight + my_height
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+print(f"My height in cm is {my_height_cm} and my weight in kg is {my_weight_kg}.")
