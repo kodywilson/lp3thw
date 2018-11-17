9ff5834a952a5bda6975378ea2e583f43592c6b9
Kody Wilson <kodywilson@gmail.com>
2018-11-16 16:36:28 -0800

    started lynda.com learn python

---
+++ lynda_learn_python/Ch1/variables_start.py
@@ -0,0 +1,20 @@
+f=0
+print(f)
+
+f="abc"
+print(f)
+
+print("this is a string " + str(123))
+
+def someFunction():
+  global f
+  f="def"
+  print(f)
+
+someFunction()
+print(f)
+
+del f
+#print(f) #won't work, f is no longer defined
+f=0 # now it will work
+print(f)
