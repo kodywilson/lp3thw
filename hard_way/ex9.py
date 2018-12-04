1f84c7f35e7a0a808557964a4140756ef2888aa5
Kody Wilson <kodywilson@gmail.com>
2018-12-03 21:19:08 -0800

    Finish exercise 9

--- hard_way/ex9.py
+++ hard_way/ex9.py
@@ -4,3 +4,9 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 print("Here are the days:", days)
 
 print("Here are the months:", months)
+
+print("""
+There's something going on here with the three double qoutes.
+
+We can type anything we want.
+""")
