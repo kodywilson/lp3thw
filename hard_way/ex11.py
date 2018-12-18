ba7d7fdce44cfb2221fb935088920642df35f0ef
Kody Wilson <kodywilson@gmail.com>
2018-12-18 06:19:18 -0800

    More exercise 11

--- hard_way/ex11.py
+++ hard_way/ex11.py
@@ -1,4 +1,6 @@
-print("How old are you?", end=' ')
+print("How old are you? ", end=' ')
 age = input()
+print("How tall are you? ", end='')
+height = input()
 
-print(f"So, you are {age} years old.")
+print(f"So, you are {age} years old and {height} tall.")
