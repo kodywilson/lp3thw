79fd5e5583922cae96910864afdce25684a48958
Kody Wilson <kodywilson@gmail.com>
2019-01-16 17:27:10 -0800

    game is built, testing now

--- invent/hangman.py
+++ invent/hangman.py
@@ -99,3 +99,49 @@ def getGuess(alreadyGuessed):
     else:
       return guess
 
+def playAgain():
+  # This function returns True if the player wants to play again.
+  print('Do you want to play again? (yes or no)')
+  return input().lower().startswith('y')
+
+print('H A N G M A N')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+  displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord)
+
+  # Let the player type in a letter.
+  guess = getGuess(missedLetters + correctLetters)
+
+  if guess in secretWord:
+    correctLetters = correctLetters + guess
+
+    # Check if the player has won
+    foundAllLetters = True
+    for i in range(len(secretWord)):
+      if secretWord[i] not in correctLetters:
+        foundAllLetters = False
+        break
+    if foundAllLetters:
+      print('Yes! The secret word is "' + secretWord + '"! You have won!')
+      gameIsDone = True
+  else:
+    missedLetters = missedLetters + guess
+
+    # Check if player has guessed too many times and lost
+    if len(missedLetters) == len(HANGMANPICS) - 1:
+      displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord)
+      print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
+      gameIsDone = True
+
+  if gameIsDone:
+    if playAgain():
+      missedLetters = ''
+      correctLetters = ''
+      gameIsDone = False
+      secretWord = getRandomWord(words)
+    else:
+      break
