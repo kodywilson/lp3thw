e9f5bf791610deeba28f1625b4c428a950abbb50
Kody Wilson <kodywilson@gmail.com>
2019-01-18 16:06:31 -0800

    Finish up exercise 14

--- hard_way/ex14.py
+++ hard_way/ex14.py
@@ -11,3 +11,9 @@ print(f"Where do you live {user_name}?")
 lives = input(prompt)
 print("What kind of computer do you have?")
 computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
