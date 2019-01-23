c1344b59e37f7a1f912e9fa1f6421eb8f83edaea
Kody Wilson <kodywilson@gmail.com>
2019-01-22 22:23:26 -0800

    more exercise 14

--- hard_way/ex14.py
+++ hard_way/ex14.py
@@ -16,4 +16,5 @@ print(f"""
 Alright, so you said {likes} about liking me.
 You live in {lives}. Not sure where that is.
 And you have a {computer} computer. Nice.
+That's all for today, have a good one.
 """)
