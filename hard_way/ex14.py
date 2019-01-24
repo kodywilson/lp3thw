6e9b1a93eed4732ebbc9eb3844ec0a35f4189aca
Kody Wilson <kodywilson@gmail.com>
2019-01-23 16:40:09 -0800

    exercise 14 extra credit

--- hard_way/ex14.py
+++ hard_way/ex14.py
@@ -2,8 +2,8 @@ import sys
 
 script = sys.argv[0]
 user_name = sys.argv[1]
-prompt = '> '
-print(f"Hi {user_name}, I'm the {script} script.)")
+prompt = ':> '
+print(f"Hi {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
