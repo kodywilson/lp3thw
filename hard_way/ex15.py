02d4d9ba7f7fa7dd37ba3b1bd05385fe9f4b86fa
Kody Wilson <kodywilson@gmail.com>
2019-01-29 12:50:15 -0800

    read file and then ask for anohter

--- hard_way/ex15.py
+++ hard_way/ex15.py
@@ -7,3 +7,9 @@ txt = open(filename)
 
 print(f"Here's your file {filename}:")
 print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+txt_again = open(file_again)
+
+print(txt_again.read())
