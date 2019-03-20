71833a90a07933456d6a5a353c9b82902ea0ce44
Kody Wilson <kodywilson@gmail.com>
2019-03-20 11:47:52 -0700

    finish main part of exercise 20

--- hard_way/ex20.py
+++ hard_way/ex20.py
@@ -25,3 +25,9 @@ print("Let's print three lines:")
 
 current_line = 1
 print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
