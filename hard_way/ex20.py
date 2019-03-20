ebb5e7596d53081b7b5e92ac30fb1bd08444d3cc
Kody Wilson <kodywilson@gmail.com>
2019-03-20 11:41:48 -0700

    testing first function

--- hard_way/ex20.py
+++ hard_way/ex20.py
@@ -11,4 +11,8 @@ def rewind(f):
 def print_a_line(line_count, f):
   print(line_count, f.read-line())
 
+current_file = open(input_file)
 
+print("First let's print the whole file:\n")
+
+print_all(current_file)
