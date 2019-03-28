f68f1ce4d632ae3e9d2973bca5561d5428c5e925
Kody Wilson <kodywilson@gmail.com>
2019-03-27 18:48:57 -0700

    subtraction function

--- hard_way/ex21.py
+++ hard_way/ex21.py
@@ -2,8 +2,14 @@ def add(a, b):
   print(f"Adding {a} + {b}")
   return a + b
 
+def subtract(a, b):
+  print(f"Subtracting {a} - {b}")
+  return a - b
+
 print("Let's do some math with just functions!")
 
 age = add(30, 5)
-
 print(f"Age: {age}")
+
+height = subtract(78, 4)
+print(f"Hight: {height}")
