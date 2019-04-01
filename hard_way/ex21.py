6f601dc246d6bb96de21041dd9bbfd9593a95fdb
Kody Wilson <kodywilson@gmail.com>
2019-04-01 15:17:12 -0700

    add multiply function

--- hard_way/ex21.py
+++ hard_way/ex21.py
@@ -6,6 +6,10 @@ def subtract(a, b):
   print(f"Subtracting {a} - {b}")
   return a - b
 
+def multiply(a, b):
+  print(f"Multiplying {a} x {b}")
+  return a * b
+
 print("Let's do some math with just functions!")
 
 age = add(30, 5)
@@ -13,3 +17,6 @@ print(f"Age: {age}")
 
 height = subtract(78, 4)
 print(f"Hight: {height}")
+
+weight = multiply(90, 2)
+print(f"Weight: {weight}")
